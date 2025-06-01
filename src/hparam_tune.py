3d8aaa328f4b29014ea7c43e05c0acb64fd8f887
KCTaka <kc.takahashi04@gmail.com>
2025-06-01 11:02:36 -0700

    Hyperparameter tuning adjustments

--- src/hparam_tune.py
+++ src/hparam_tune.py
@@ -15,10 +15,24 @@ import wandb
 from src.utils.hydra import instantiate_list
 from src.systems.autoencoder import AutoEncoder
 
-def train(cfg: DictConfig, model_ae: AutoEncoder = None):
+def train(main_cfg: DictConfig, model_ae: AutoEncoder = None):
     if model_ae is None:
         raise ValueError("model_ae must be provided as an argument to train function.")
     
+    run = wandb.init(
+        project=main_cfg.loggers.wandb.project,
+        job_type="sweep"
+    )
+    
+    model_ae = load_pretrained_ae(
+        run=run,
+        artifact_path="enter artifact here",
+        models_cfg=main_cfg.models
+    )
+    
+    sweep_cfg = flat_dict_to_nested_dict(wandb.config)
+    cfg = OmegaConf.merge(main_cfg, sweep_cfg)
+
     datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
     
     system_ddpm: LightningModule = hydra.utils.instantiate(cfg.ddpm, model_ae=model_ae)
@@ -37,24 +51,37 @@ def train(cfg: DictConfig, model_ae: AutoEncoder = None):
         model=system_ddpm,
         datamodule=datamodule,
     )
-
-@hydra.main(version_base=None, config_path="../configs", config_name="hparam_tune")
-def main(cfg: DictConfig):
-    cfg = OmegaConf.to_container(cfg, resolve=True)
     
-    ## Load the autoencoder weights
-    model_ae = hydra.utils.instantiate(cfg.models.autoencoder)
-    model_d = hydra.utils.instantiate(cfg.models.discriminator)
-    lpips = hydra.utils.instantiate(cfg.models.perceptual)
-    system_ae = AutoEncoder.load_from_checkpoint(cfg.autoencoder_weights, strict=False, model_ae=model_ae, model_d=model_d, lpips=lpips)
+def flat_dict_to_nested_dict(flat_dict):
+    nested_dict = OmegaConf.create()
+    for key, value in flat_dict.items():
+        OmegaConf.update(nested_dict, key, value, merge=True)
+    return nested_dict
+
+def load_pretrained_ae(run, artifact_path: str, models_cfg: DictConfig):
+    model_ae = hydra.utils.instantiate(models_cfg.autoencoder)
+    model_d = hydra.utils.instantiate(models_cfg.discriminator)
+    lpips = hydra.utils.instantiate(models_cfg.perceptual)
+    
+    artifact = run.use_artifact(artifact_path, type="model")
+    artifact_dir = artifact.download()
+    print(f"Artifact downloaded to: {artifact_dir}")
+    system_ae = AutoEncoder.load_from_checkpoint(artifact_dir, strict=False, model_ae=model_ae, model_d=model_d, lpips=lpips)
     model_ae = system_ae.model_ae.eval()
     
-    train_ddpm = lambda cfg: train(cfg, model_ae=model_ae)
-    
+    return model_ae
+
+@hydra.main(version_base=None, config_path="../configs", config_name="train_ddpm")
+def main(cfg: DictConfig):    
+    train_ddpm = lambda: train(main_cfg=cfg)
+
+    sweep_cfg = OmegaConf.to_container(OmegaConf.load("./configs/sweep/wandb.yaml"), resolve=True)
+
     sweep_id = wandb.sweep(
-        cfg,
+        sweep_cfg,
         project=cfg.loggers.wandb.project,
     )
+    
     wandb.agent(
         sweep_id,
         function=train_ddpm,
